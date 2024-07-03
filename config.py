11b4c925a8313dcde5d2cc7d453a43a8e6feeb30
Luxzzifer <135999889+Luxzzifer@users.noreply.github.com>
2024-07-03 19:31:02 +0700

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-MONGO_URI = "mongodb+srv://liklikwatilyedijaezra:hS39OQdtNYulIMqC@cluster0.734mex6.mongodb.net/"
-DB_WAREHOUSE = 'DB_Warehouse'
-COLLECTION_PRODUCT = 'Product'
-COLLECTION_PRODUCT_KELUAR = 'ProductKeluar'
+MONGO_URI = ""
+DB_WAREHOUSE = ''
+COLLECTION1 = ''
